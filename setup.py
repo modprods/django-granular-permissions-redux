1bbbe1c9a238f754c3d1b7b60a3843fa162f243c
Michela Ledwidge <michela@users.noreply.github.com>
2016-02-14 21:33:49 +1100

    update setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="django_granular_permissions",
-	version="0.3",
+	version="0.4",
 	description="Django Granular Permissions allow you to setup per-row permissions.",
     author="Yazzg",
 	author_email="yazzg...@gmail.com",
-	maintainer="Rob Yates",
-	maintainer_email="",
-	url="https://github.com/ryates/django-granular-permissions-redux",
+	maintainer="Michela Ledwidge",
+	maintainer_email="michela@modprods.com",
+	url="https://github.com/modprods/django-granular-permissions-redux",
 	packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
